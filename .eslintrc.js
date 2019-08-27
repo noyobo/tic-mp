9a26050f355087897a0ab4dd348c7ca2062dcab1
noyobo <noyobo@gmail.com>
2019-08-27 18:42:53 +0800

    ci: eslint && prettier

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    es6: true,
+    browser: false,
+    commonjs: true,
+    node: true,
+  },
+  globals: {
+    File: true, // browser
+    Promise: false,
+    wx: true,
+    App: true,
+    Page: true,
+    Component: true,
+    requirePlugin: true,
+    Behavior: true,
+    getDate: true,
+    getApp: true,
+    getCurrentPages: true,
+  },
+  extends: ['prettier', 'plugin:promise/recommended', 'eslint:recommended'],
+  plugins: ['promise'],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': 'off',
+    'no-unused-vars': ['error', { vars: 'all', args: 'none', ignoreRestSiblings: true }],
+    'promise/always-return': 'error',
+  },
+};
